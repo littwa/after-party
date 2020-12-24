1aba0cf5367f07b9b55eb06320d8ad7c6f93b82a
littwa <littwa777@gmail.com>
2020-12-24 12:34:13 +0200

    first1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # after-party
+  <!-- "dependencies": {
+    "dotenv": "^8.2.0",
+    "express": "^4.17.1",
+    "morgan": "^1.10.0"
+  } -->
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let f = 5;
-console.log(f + 1);
+// const express = require("express");
+// const server = express();
+
+// // const morgan = require("morgan");
+// // server.use(morgan("combined"));
+
+// // require("dotenv").config();
+// // console.log(process.env.q);
+
+// server.get("/", async (req, res, next) => res.send("ok"));
+
+// server.listen(3000, () => console.log("Started Port", 3000));
+//==============================Test del===========================
